b299652e414e84387fd47171f17bafdcc2782414
furukawa1020 <f.kotaro.0530@gmail.com>
2025-11-29 15:58:41 +0900

    feat: Add a new input handling module to manage keyboard states and events.

--- src/system/input.lua
+++ src/system/input.lua
@@ -3,6 +3,26 @@ local Input = {}
 Input.keys = {}
 Input.keysPressed = {}
 
+function Input.init()
+    -- Initialize input mappings if needed
+end
+
+function Input.update()
+    -- Reset single-frame input states
+    Input.keysPressed = {}
+end
+
+function Input.keypressed(key)
+    if key == "up" or key == "down" or key == "left" or key == "right" or key == "return" or key == "escape" or key == "tab" then
+        Input.keysPressed[key] = true
+    end
+end
+
+function Input.isDown(key)
+    return love.keyboard.isDown(key)
+end
+
+function Input.wasPressed(key)
     return Input.keysPressed[key]
 end
 
